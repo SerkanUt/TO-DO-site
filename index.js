74ffa1018cbc501f21eac3dc5295aecd6a8f95d3
SerkUt <utogluserkan@gmail,.com>
2022-07-25 20:43:12 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const todoList = [];
+
+const todoListElement = document.querySelector("#myUL");
+
+document.querySelector('#todo_button').addEventListener('click', addTodo);
+
+function addTodo(){
+    const todoText = document.querySelector("#myInput").value;
+
+    const todoObject = {
+        id: todoList.length,
+        todoText: todoText,
+        isDone: false,
+    };
+
+    todoList.push(todoObject);
+    displayTodos()
+}
+
+function doneTodo(todoId){
+    const selectedTodoIndex = todoList.findIndex((item) => item.id == todoId)
+
+    todoList[selectedTodoIndex].isDone = true;
+
+    displayTodos()
+
+}
+
+function displayTodos(){
+    todoListElement.innerHTML = "";
+    
+    todoList.forEach((item) => {
+        const todoElement = document.createElement("li");
+
+        todoElement.innerHTML = item.todoText;
+        todoElement.setAttribute("data-id", item.id);
+
+        if (item.isDone) {
+            todoElement.classList.add("checked");
+        }
+
+        todoElement.addEventListener('click', function(e){
+            const selectedId = e.target.getAttribute("data-id");
+            doneTodo(selectedId);
+        })
+
+        todoListElement.appendChild(todoElement);
+        
+    })
+}
+
+displayTodos()
